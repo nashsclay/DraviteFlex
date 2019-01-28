83342c189cb32f43d56db29db743d740f4e18802
nashsclay <nashsclay@gmail.com>
2019-01-27 21:16:01 -0700

    Update DraviteFlex_MN_Setup.sh

--- DraviteFlex_MN_Setup.sh
+++ DraviteFlex_MN_Setup.sh
@@ -2,17 +2,17 @@
 #!/bin/bash
 
 TMP_FOLDER=$(mktemp -d)
-CONFIG_FILE='CoinAlpaca.conf'
-CONFIGFOLDER='/root/.CoinAlpaca'
-COIN_DAEMON='CoinAlpacad'
-COIN_CLI='CoinAlpaca-cli'
-COIN_TX='CoinAlpaca-tx'
+CONFIG_FILE='draviteflex.conf'
+CONFIGFOLDER='/root/.draviteflex'
+COIN_DAEMON='draviteflexd'
+COIN_CLI='draviteflex-cli'
+COIN_TX='draviteflex-tx'
 COIN_PATH='/usr/local/bin/'
 COIN_TGZ='https://github.com/Coin-Alpaca/CoinAlpaca-Core/releases/download/1.0.0.1/CoinAlpaca-Linux-Daemon.zip'
 COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
-COIN_NAME='CoinAlpaca'
-COIN_PORT=22807
-RPC_PORT=9332
+COIN_NAME='DraviteFlex'
+COIN_PORT=22050
+RPC_PORT=42781
 
 NODEIP=$(curl -s4 icanhazip.com)
 
